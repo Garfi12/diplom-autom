c8a0544e0cc05901e1ef8656400dfdf561d3d646
Max Kuz <m001_01@mail.ru>
2025-09-13 00:35:49 +0500

    diplom

---
+++ constants.py
@@ -0,0 +1,8 @@
+UI_url = "https://www.chitai-gorod.ru"
+API1_url = "https://web-agr.chitai-gorod.ru/web/api/v1/cart/product"
+API2_url = "https://web-agr.chitai-gorod.ru/web/api/v1/cart"
+bearer_token = ("Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3VzZXItcmlnaHQiLCJzdWIiOjIyNTcxMzM3LCJpYXQiOjE3NTc3MDUwMzYsImV4cCI6MTc1NzcwODYzNiwidHlwZSI6MjAsImp0aSI6IjAxOTkzZjYyLTYxZjYtNzhkZi1hYjlhLWVkNWJlNWFmZjM0MyIsInJvbGVzIjoxMH0.Vt5DUsi0Jrs1wwIW86h0WNJQ0zhiCPVMxrdV-I5tCQ8")
+User_Agent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+              "AppleWebKit/537.36 (KHTML, like Gecko) "
+              "Chrome/140.0.0.0 Safari/537.36")
+User_ID = "1d124fcd-bb52-49fa-80ba-d30e6a7c241a"
